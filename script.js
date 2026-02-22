517c99535dc9c08252cee8caad4399906c6082ad
leirsam <leirsam@gmail.com>
2026-02-22 17:50:23 +0000

    added the checkWin function and great game as well - still need to undestand how to move on

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-const gameBoard (() => {
-    board = array(9).fill("");
+function gameBoard() {
+    const board = Array(9).fill("");
     return {board};
-})();
+}
 
+function createPlayer(name, symbol) {
+    return {name, symbol};
+}
+
+function createGame(name) {
+    const player = createPlayer(name);
+    const game = gameBoard();
+    return {player, game};
+}
+
+function checkWin(board, symbol) {
+    
+    const wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [7, 8, 9],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    
+    return wins.some(([a ,b ,c]) => (   board[a] === symbol && 
+                                        board[b] === symbol && 
+                                        board[c] === symbol))
+
+    }
